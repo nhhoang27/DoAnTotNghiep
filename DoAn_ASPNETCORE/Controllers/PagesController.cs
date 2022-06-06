db061f549a8ad789432fde2e6179f4ff11e5a24f
nhhoang27 <bito272000@gmail.com>
2022-06-06 08:04:50 +0700

    fix phan trang

--- DoAn_ASPNETCORE/Controllers/PagesController.cs
+++ DoAn_ASPNETCORE/Controllers/PagesController.cs
@@ -95,7 +95,6 @@ namespace DoAn_ASPNETCORE.Controllers
             ViewBag.id = id;
             ViewBag.Username = HttpContext.Session.GetString("username");
             ViewBag.Page = page ?? 1;
-
             return View();
         }
         //public IActionResult Login()
